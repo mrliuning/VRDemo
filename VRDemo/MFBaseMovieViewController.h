49dc895a207169103061ceddb46217150cd9edce
liuning1084126465 <1084126465@qq.com>
2017-06-16 13:10:34 +0800

    代码

---
+++ VRDemo/MFBaseMovieViewController.h
@@ -0,0 +1,15 @@
+//
+//  MFBaseMovieViewController.h
+//  VRDemo
+//
+//  Created by liuning on 16/6/30.
+//  Copyright © 2016年 ME Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MFBaseMovieViewController : UIViewController
+
+- (id)initWithUrl:(NSURL *)playUrl;
+
+@end
